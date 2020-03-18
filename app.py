443232823a3096ea3b6d5d1554e5bfac0f3e7915
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-03-18 23:46:41 +0900

    ラースドラゴンの残りHPをテンプレートマッチングで求めるプログラムを作成

---
+++ app.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import os
+import re
+import click
+import time
+import cv2
+import win32gui
+import ctypes
+import math
+from PIL import ImageGrab
+import sys
+from datetime import datetime
+# print出力に色付ける
+from termcolor import colored
+# 関数、定数をインポート
+from commons import *
+
+@click.command()
+@click.option('--development','-dev',is_flag=True)
+def main(development):
+
+    # プリコネを起動してハンドルを取得
+    prkn_handle = execute_prkn()
+
+    # とりあえず1秒毎に画面をキャプチャ
+    try:
+        while(True):
+            time.sleep(1)
+
+            rect_left, rect_top, rect_right, rect_bottom = win32gui.GetWindowRect(prkn_handle)
+
+            # ウィンドウの外枠＋数ピクセル余分にとれちゃうので1280x960の位置補正
+            cap_left, cap_top, cap_right, cap_bottom = ajust_capture_position(rect_left, rect_top, rect_right, rect_bottom)
+
+            # 指定した領域内をクリッピング
+            current_time = datetime.now().strftime('%Y%m%d%H%M%S%f')
+            img = ImageGrab.grab(bbox=(cap_left, cap_top, cap_right, cap_bottom))
+            if development:
+                img.save(OUTPUT_DIR + current_time + '.png')
+            original_frame = np.array(img)
+
+            # スコアの数字についてテンプレートマッチング
+            remain = analyze_hp(original_frame)
+
+            if (remain):
+                enemy_hp = calc_remain(remain)
+                enemy_hp_size = math.floor(enemy_hp / 100)
+                progress_bar_size = math.floor(EX3_BOSS_HP / 100)
+                progress_bar = ('=' * enemy_hp_size) + (' ' * (progress_bar_size - enemy_hp_size))
+                print('\r残りHP:[{0}] {1} / {2}'.format(progress_bar, str(enemy_hp).rjust(4, ' '), str(EX3_BOSS_HP).rjust(4, ' ')), end='')
+
+    except KeyboardInterrupt:
+        print(colored("プログラムを終了します", "green"))
+        exit(0)
+
+if __name__ == '__main__':
+    main()
+
---
+++ commons.py
@@ -0,0 +1,99 @@
+
+import os
+import time
+import cv2
+import subprocess
+from datetime import datetime
+import math
+from PIL import Image
+import win32gui
+import tkinter, tkinter.filedialog, tkinter.messagebox
+# print出力に色付ける
+from termcolor import colored
+import colorama
+colorama.init()
+# 設定ファイルを利用する
+import configparser
+
+# 変数(定数扱いする変数)
+PRKN_WINDOW_NAME = 'PrincessConnectReDive'
+CONFIG_FILE_NAME = 'settings.ini'
+CONFIG_SECTION_NAME = 'config'
+OUTPUT_DIR = os.path.abspath(os.path.dirname(__file__)) + os.sep + 'output' + os.sep
+SAMPLE_DIR = os.path.abspath(os.path.dirname(__file__)) + os.sep + 'sample_data' + os.sep
+CURRENT_HP_TEMPLATE = SAMPLE_DIR + 'current_hp.png'
+
+# ボスのHP(万)
+EX3_BOSS_HP = 3500
+
+# ボスのHPバーのROI
+BOSS_HP_ROI = (380, 40, 902, 48)
+BOSS_HP_WIDTH = BOSS_HP_ROI[2] - BOSS_HP_ROI[0]
+
+# ボスのHPの現在値のサンプルデータ(オレンジと黒の境界の場所)
+BOSS_HP_LOWER_BGR = (9, 43 ,123)
+BOSS_HP_UPPER_BGR = (150, 180, 248)
+BINARY_BOSS_HP_REMAIN = cv2.imread(CURRENT_HP_TEMPLATE)
+BINARY_BOSS_HP_REMAIN = cv2.inRange(BINARY_BOSS_HP_REMAIN, BOSS_HP_LOWER_BGR, BOSS_HP_UPPER_BGR)
+
+
+def execute_prkn():
+    # DMM版プリコネを起動してハンドルを返す
+    prkn_handle = win32gui.FindWindow(None, PRKN_WINDOW_NAME)
+    if prkn_handle <= 0:
+
+        subprocess.Popen('start dmmgameplayer://priconner/cl/general/priconner', shell=True)
+        time.sleep(5)
+
+        # ウィンドウ名でハンドル取得
+        while(True):
+            prkn_handle = win32gui.FindWindow(None, PRKN_WINDOW_NAME)
+            if prkn_handle > 0:
+                break
+
+            print(colored("プリコネが起動してないよー", "red"))
+            time.sleep(3)
+
+    print(colored("プリコネが起動してるよー", "green"))
+    print(colored("Ctrl+Cで終了します", "green"))
+    return prkn_handle
+
+
+def ajust_capture_position(rect_left,rect_top,rect_right,rect_bottom):
+    # キャプチャ位置修正
+    cap_left = rect_left + 8
+    cap_top = rect_top + 32
+    cap_right = cap_left + 1280
+    cap_bottom = cap_top + 720
+    return cap_left, cap_top, cap_right, cap_bottom
+
+
+def analyze_hp(original_frame):
+    # 残りHP解析
+    # ダメージを与えた瞬間の爆発エフェクトをキャプチャしたときの二値化がうまく行えてない模様。。
+    # 先にHPバーの外枠も含めたマッチングとかした方がいいのかもしれない
+
+    # HPバーの部分を切り取り
+    work_frame = original_frame[BOSS_HP_ROI[1]:BOSS_HP_ROI[3], BOSS_HP_ROI[0]:BOSS_HP_ROI[2]]
+    Image.fromarray(work_frame).save(OUTPUT_DIR + 'hoge1.png')
+
+    # 二値化
+    work_frame = cv2.cvtColor(work_frame, cv2.COLOR_RGB2BGR)
+    work_frame = cv2.inRange(work_frame, BOSS_HP_LOWER_BGR, BOSS_HP_UPPER_BGR)
+    cv2.imwrite(OUTPUT_DIR + 'hoge2.png', work_frame)
+
+    # 残りHPの位置をテンプレートマッチングにより取得
+    remain = None
+    res = cv2.matchTemplate(work_frame, BINARY_BOSS_HP_REMAIN, cv2.TM_CCORR_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+#     print(min_val, max_val, min_loc, max_loc)
+    if (min_loc[0] > 0 and max_val > 0.85):
+        remain = min_loc[0]
+
+    return remain
+
+def calc_remain(remain):
+    # 残りHP計算
+    # テンプレート画像のHPバーの長さ522pxと、取得した残りHP位置の比率からおおよその残りHPを出す
+    result = math.floor(remain / BOSS_HP_WIDTH * EX3_BOSS_HP)
+    return result
